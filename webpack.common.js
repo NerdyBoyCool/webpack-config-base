25f066899752045ab9fc526482e62918f3e1478a
daikitakaya <takaya@sonicgarden.jp>
2020-11-26 19:39:02 +0900

    install babel

--- webpack.common.js
+++ webpack.common.js
@@ -11,6 +11,15 @@ module.exports = {
     filename: 'js/[name].[contenthash].js',
     chunkFilename: 'js/[name].[contenthash].js'
   },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      }
+    ]
+  },
   optimization: {
     splitChunks: {
       chunks: 'initial',
